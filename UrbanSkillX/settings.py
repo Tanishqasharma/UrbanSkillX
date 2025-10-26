40686b4d649ace414c8eb4764ac55433c72177ce
sayan-academic <sayanacademic69@gmail.com>
2025-10-26 06:56:17 +0530

    preparing for deploy

--- UrbanSkillX/settings.py
+++ UrbanSkillX/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-nm(pwb8js^8j&8apltlww(f3_@+)kr&i5q&$$inb&lyp2!hd46'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
